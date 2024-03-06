80b315ef944efc2de0f6e3171a19e3788b673050
Jay McLain <jay_mclain@msn.com>
2024-03-06 08:55:07 -0600

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.greeting}>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  greeting: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
+  }
+});
